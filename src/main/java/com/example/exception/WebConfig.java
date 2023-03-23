4cc84d50d73f9b2d1060804bf8e1aa64fa7ba16e
minseok <alstjr706@naver.com>
2023-03-23 17:38:04 +0900

    git : 예외처리 예제

---
+++ src/main/java/com/example/exception/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.exception;
+
+import com.example.exception.interceptor.LogInterceptor;
+import com.example.exception.resolver.MyHandlerExceptionResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).order(1).addPathPatterns("/**").excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**" //오류 페이지 경로
+        );
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+}
